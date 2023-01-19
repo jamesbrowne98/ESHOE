9aa796038f686a584c4b0152090baf20fa3ce69b
jamesbrowne98 <james.browne@students.plymouth.ac.uk>
2023-01-19 00:59:39 +0000

    Mongoose working
    got the database displaying data on the page

--- ESHOE/models/shoes.js
+++ ESHOE/models/shoes.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const shoeSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Types.ObjectId,
-    required: true
-},
-  name: String,
-  size: [Number],
-  price: Number
+const shoeSchema = new Schema({
+    name: { type: String, required: true },
+    size: { type: Number, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('shoes', shoeSchema);
+module.exports = mongoose.model('Shoe', shoeSchema);
--- ESHOE/server.js
+++ ESHOE/server.js
@@ -1,33 +1,27 @@
 ﻿const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const ejs = require('ejs');
 const path = require('path');
+const Shoes = require('./models/shoes');
+const router = require('./routes/shoes');
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"));
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect('mongodb+srv://jamesbrowne:tGkr76p5m8cgfGUG@cluster0.90fwvtk.mongodb.net/Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
 mongoose.set('strictQuery', false);
-
-const shoesRouter = require('./routes/shoes');
-app.use('/shoes', shoesRouter);
-
-app.get('/', async (req, res) => {
-    try {
-      const shoes = await Shoes.find();
-      res.render('shop', { shoes });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
+mongoose.connect('mongodb+srv://jamesbrowne:tGkr76p5m8cgfGUG@cluster0.90fwvtk.mongodb.net/Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to Database');
+  })
+  .catch(err => {
+    console.log(`Error: ${err.message}`);
   });
-app.use(express.static('public'));
-app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
-});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
